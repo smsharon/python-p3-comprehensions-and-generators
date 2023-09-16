fb60719860fa36eda7bf09902099fc5a3a49d6b8
Shallon Mokeira Said <shallonmokeira@gmail.com>
2023-09-16 11:34:46 -0400

    completed assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_numbers = [n for n in num_list if n % 2 == 0]
+    return even_numbers
 
 def make_exclamation(sentence_list):
-    pass
+    exclamation = [sentence + "!" for sentence in sentence_list]
+    return exclamation
